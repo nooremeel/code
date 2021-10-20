a75f0d5dcc8d4ebc723ebc0459344c6ae821d79f
nour <nooremeel1@gmail.com>
2021-10-20 09:40:08 +0200

    first commit

---
+++ nour/Source1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "input two numbers \n";
+	cin >> x >> y;
+	cout << "the sum is \n" << x + y <<"\n";
+	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+}
